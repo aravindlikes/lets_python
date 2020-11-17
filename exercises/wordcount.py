b8777959b2fc6023d3f4b707d1499aa0ee9eeccf
Aravind <aravindanarayanan.m@gmail.com>
2020-11-17 16:33:34 +0530

    Week - 1 List, Tuples, Set,Dict and Files

--- exercises/wordcount.py
+++ exercises/wordcount.py
@@ -36,6 +36,7 @@ print_words() and print_top().
 
 import sys
 
+
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
 # You could write a helper utility function that reads a file
@@ -46,20 +47,56 @@ import sys
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
-def main():
-  if len(sys.argv) != 3:
-    print('usage: ./wordcount.py {--count | --topcount} file')
-    sys.exit(1)
+def sort_value(item):
+    return item[-1]
 
-  option = sys.argv[1]
-  filename = sys.argv[2]
-  if option == '--count':
-    print_words(filename)
-  elif option == '--topcount':
-    print_top(filename)
-  else:
-    print('unknown option: ' + option)
-    sys.exit(1)
+
+def build_dict(filename):
+    f = open(filename, 'r')
+    words = f.read().split()
+    count = {}
+    for word in words:
+        word = word.lower()
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
+    f.close()
+    return count
+
+
+def print_words(filename):
+    dict = build_dict(filename)
+
+    for word in sorted(dict.keys()):
+        print(word, dict[word])
+
+
+def print_top(filename):
+    count = build_dict(filename)
+    i = 0
+
+    items = sorted(count.items(), key=sort_value, reverse=True)
+    for item in items[:20]:
+        print(item[0] + ': ' + str(item[1]) + ' times')
+        i += 1
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
+
 
 if __name__ == '__main__':
-  main()
+    main()
